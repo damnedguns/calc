204f9799f77836b28f90581f0eed19722c7f0568
damnedguns <47381395+damnedguns@users.noreply.github.com>
2019-03-06 12:33:47 +0100

    addition added

--- calc_js.js
+++ calc_js.js
@@ -16,7 +16,21 @@ buttons.forEach((button) => {
     button.addEventListener('click', function () {
         let selection = button.textContent;
         ClickBoutons(selection);
+
+
+        if (selection=="=") {
+        result = display.textContent.slice(0, -1);
+        var egalite = result;
+        display.textContent = eval(result);
+        result = "";
+        unset(result);   
+        }
+
     })
+
+
+
+
 })
 
 
